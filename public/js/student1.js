8c2e38138221cc6fecd01e2c6593c1d6e237e520
emmettmallon <emmettmallon@u.boisestate.edu>
2024-11-20 13:25:42 -0700

    updated with student1 branch

--- public/js/student1.js
+++ public/js/student1.js
@@ -1,1 +1,6 @@
 
+document.getElementById('deleteButton').addEventListener('click', function() {
+    fetch('/click', { method: 'POST' })
+        .then(response => response.text())
+        .then(data => console.log(data));
+});``
--- src/server.js
+++ src/server.js
@@ -6,6 +6,10 @@ const sqlite3 = sql.verbose()
 // Create an in-memory SQLite database
 const db = new sqlite3.Database(':memory:')
 
+db.run(`CREATE TABLE student1 (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  comment TEXT NOT NULL)`)
+
 const app = express()
 app.use(express.static('public'))
 app.set('views', 'views')
@@ -34,29 +38,61 @@ app.get('/', function (req, res) {
   res.render('index')
 })
 
-// Route for student1 page
+/////////////// 
+// Student 1 //
+
 app.get('/student1', function (req, res) {
   console.log('GET called for student1')
   res.render('student1')
 })
 
-// Route for student2 page (updated to fetch comments)
-app.get('/student2', function (req, res) {
-  console.log('GET called for student2')
-
-  // Fetch up to 5 random comments from the database for student2
-  db.all("SELECT text FROM comments ORDER BY RANDOM() LIMIT 5", function(err, rows) {
-    if (err) {
-      console.error(err)
-      res.status(500).send("Error retrieving comments.")
-    } else {
-      // Pass the comments to the student2 Pug template
-      res.render('student2', { comments: rows })
-    }
+app.get('/s1comments', function (req, res) {
+  const local = { comments: [] }
+  db.each('SELECT id, comment FROM student1', function (err, row) {
+      if (err) {
+          console.log(err)
+      } else {
+          local.comments.push({ id: row.id, comment: row.comment })
+      }
+  }, function (err, numrows) {
+      if (!err) {
+          res.render('s1comments', local)
+      } else {
+          console.log(err)
+      }
   })
+  console.log('GET called')
 })
 
-// Route for student3 page
+app.post('/s1add', function (req, res) {
+  console.log('adding comment: ' + req.body.comment)
+  const stmt = db.prepare('INSERT INTO student1 (comment) VALUES (?)')
+  stmt.run(req.body.comment)
+  stmt.finalize()
+})
+
+app.post('/s1delete', function (req, res) {
+  console.log('deleting comment')
+  const stmt = db.prepare('DELETE FROM student1 where id = (?)')
+  stmt.run(req.body.id)
+  stmt.finalize()
+  console.log('deleted comment')
+
+})
+
+///////////////
+// Student 2 //
+
+app.get('/student2', function (req, res) {
+  console.log('GET called')
+  res.render('student2')
+})
+
+
+///////////////
+// Student 3 //
+
+
 app.get('/student3', function (req, res) {
   console.log('GET called for student3')
   res.render('student3')
