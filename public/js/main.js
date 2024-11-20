3460207b16000375bb491195831d7ad0432351da
Nathan Marquis <nathanmarquis659@u.boisestate.edu>
2024-11-20 16:59:42 -0700

    Text finished, now fixing comments edit feature

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,2 @@
 // TODO: Add any JavaScript here to make your page fancy :)
 
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
-  }
-  
-function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-} 
