faa39f19a6b514b6e384f37c8f30f6cbb5ed7f35
Nathan Marquis <nathanmarquis659@u.boisestate.edu>
2024-11-20 20:43:37 -0700

    Fully working! FREAKING PENCIL CURSOR!!!

--- public/js/student1.js
+++ public/js/student1.js
@@ -1,9 +1,11 @@
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
-    document.getElementById("open-button").style.display = "none";
+function openForm(id) {
+  console.log("open: " + id);
+  document.getElementById("myForm" + id).style.display = "block";
+  document.getElementById("open-button" + id).style.display = "none";
   }
   
-function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-    document.getElementById("open-button").style.display = "block";
+function closeForm(id) {
+  console.log("close: " + id);
+  document.getElementById("myForm" + id).style.display = "none";
+  document.getElementById("open-button" + id).style.display = "block";
 } 
