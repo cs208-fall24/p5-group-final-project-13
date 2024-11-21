f72d65e50e18d936eef8de516d8f18cdfcb6e2a6
emmettmallon <emmettmallon@u.boisestate.edu>
2024-11-20 18:49:33 -0700

    comments page

--- public/js/student2.js
+++ public/js/student2.js
@@ -2,4 +2,4 @@ document.getElementById('deleteButton').addEventListener('click', function() {
     fetch('/click', { method: 'POST' })
         .then(response => response.text())
         .then(data => console.log(data));
-});``
+});
