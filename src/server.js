88b7bbc6ad695febaa23f917721e2b29615340f0
emmettmallon <emmettmallon@u.boisestate.edu>
2024-11-23 12:39:30 -0700

    got rid of test comments

--- src/server.js
+++ src/server.js
@@ -11,26 +11,16 @@ db.run(`CREATE TABLE student1 (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   comment TEXT NOT NULL)`)
 
-  db.run(`CREATE TABLE student2 (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    comment TEXT NOT NULL)`)
+// Student2 table
+db.run(`CREATE TABLE student2 (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  comment TEXT NOT NULL)`)
   
 const app = express()
 app.use(express.static('public'))
 app.set('views', 'views')
 app.set('view engine', 'pug')
 app.use(express.urlencoded({ extended: false }))
-
-// Seed some initial comments into the database (for testing)
-db.serialize(() => {
-  const stmt = db.prepare("INSERT INTO student2 (comment) VALUES (?)")
-  stmt.run('This is an amazing program!')
-  stmt.run('I love learning about basket weaving underwater.')
-  stmt.run('The best major ever!')
-  stmt.run('I can\'t wait to dive in and start weaving.')
-  stmt.run('This is a unique and challenging field of study.')
-  stmt.finalize()
-})
 
 // student3 table 
 db.run(`CREATE TABLE student3 (
@@ -116,66 +106,67 @@ app.post('/s1delete', function (req, res) {
   console.log('deleted comment')
 })
 
-// Student 2 //
+////////////////////
+//   student 2    //
 
-app.get('/student2', function (req, res) {
-  const local = { comments: [] }
-  db.each('SELECT * FROM student2 ORDER BY id LIMIT 5;', function (err, row) {
-      if (err) {
-          console.log(err)
-      } else {
-          local.comments.push({ id: row.id, comment: row.comment })
-      }
-  }, function (err, numrows) {
-      if (!err) {
-          res.render('student2', local)
-      } else {
-          console.log(err)
-      }
-  })
-  console.log('GET called for student2')
-})
-
-app.get('/s2comments', function (req, res) {
-  const local = { comments: [] }
-  db.each('SELECT id, comment FROM student2', function (err, row) {
-      if (err) {
-          console.log(err)
-      } else {
-          local.comments.push({ id: row.id, comment: row.comment })
-      }
-  }, function (err, numrows) {
-      if (!err) {
-          res.render('s2comments', local)
-      } else {
-          console.log(err)
-      }
-  })
-  console.log('Comments page GET called for student2')
-})
-
-app.post('/s2add', function (req, res) {
-  console.log('adding comment: ' + req.body.id)
-  const stmt = db.prepare('INSERT INTO student2 (comment) VALUES (?)')
-  stmt.run(req.body.comment)
-  stmt.finalize()
-})
-
-app.post('/s2edit', function (req, res) {
-  console.log('editing comment: ' + req.body.id)
-  const stmt = db.prepare('UPDATE student2 SET comment = (?) WHERE id = (?)')
-  stmt.run(req.body.comment, req.body.id)
-  stmt.finalize()
-  console.log('edited comment')
-})
-
-app.post('/s2delete', function (req, res) {
-  console.log('deleting comment')
-  const stmt = db.prepare('DELETE FROM student2 where id = (?)')
-  stmt.run(req.body.id)
-  stmt.finalize()
-  console.log('deleted comment')
-})
+    app.get('/student2', function (req, res) {
+      const local = { comments: [] }
+      db.each('SELECT * FROM student2 ORDER BY id LIMIT 5;', function (err, row) {
+          if (err) {
+              console.log(err)
+          } else {
+              local.comments.push({ id: row.id, comment: row.comment })
+          }
+      }, function (err, numrows) {
+          if (!err) {
+              res.render('student2', local)
+          } else {
+              console.log(err)
+          }
+      })
+      console.log('GET called for student2')
+    })
+    
+    app.get('/s2comments', function (req, res) {
+      const local = { comments: [] }
+      db.each('SELECT id, comment FROM student2', function (err, row) {
+          if (err) {
+              console.log(err)
+          } else {
+              local.comments.push({ id: row.id, comment: row.comment })
+          }
+      }, function (err, numrows) {
+          if (!err) {
+              res.render('s2comments', local)
+          } else {
+              console.log(err)
+          }
+      })
+      console.log('Comments page GET called for student2')
+    })
+    
+    app.post('/s2add', function (req, res) {
+      console.log('adding comment: ' + req.body.id)
+      const stmt = db.prepare('INSERT INTO student2 (comment) VALUES (?)')
+      stmt.run(req.body.comment)
+      stmt.finalize()
+    })
+    
+    app.post('/s2edit', function (req, res) {
+      console.log('editing comment: ' + req.body.id)
+      const stmt = db.prepare('UPDATE student2 SET comment = (?) WHERE id = (?)')
+      stmt.run(req.body.comment, req.body.id)
+      stmt.finalize()
+      console.log('edited comment')
+    })
+    
+    app.post('/s2delete', function (req, res) {
+      console.log('deleting comment')
+      const stmt = db.prepare('DELETE FROM student2 where id = (?)')
+      stmt.run(req.body.id)
+      stmt.finalize()
+      console.log('deleted comment')
+    })
 
 
 
